bd5586d184fd86f1da6d24bf0881a98928b221b4
Bence <arvai.bence1@gmail.com>
2019-02-14 11:26:41 +0100

    Give either a movie or a TV show based on user input

--- app.py
+++ app.py
@@ -11,26 +11,39 @@ tmdb.API_KEY = '3bd3415ab14ed2107bb5f34db8b8c587'   #TODO save to env. variable
 @app.route('/')
 def index():
     genre = tmdb.Genres()
-    genres = genre.movie_list()['genres']
-    return render_template('index.html', genres=genres)
+    movie_genres = genre.movie_list()['genres']
+    tv_show_genres = genre.tv_list()['genres']
+    return render_template('index.html', movie_genres=movie_genres, tv_show_genres=tv_show_genres)
 
 
-@app.route('/get/movie')
-def api_test():
-    # pp = pprint.PrettyPrinter(indent=4)
-    movies = fill_movie_list(5)
+@app.route('/get/movie', methods=['GET', 'POST'])
+def get_movie():
+    movies = fill_to_watch_list(True)
     movie = get_random_movie(movies)
     return jsonify(movie)
 
 
-def fill_movie_list(pageCount):
-    discover = tmdb.Discover()
-    movies = []
-    for i in range(pageCount):
-        movies.append(discover.movie(page=i + 1)['results'])
-    movies = list(itertools.chain.from_iterable(movies))
+@app.route('/get/tv', methods=['GET', 'POST'])
+def get_show():
+    shows = fill_to_watch_list(False)
+    show = get_random_movie(shows)
+    show['release_date'] = show.pop('first_air_date')
+    show['title'] = show.pop('name')
+    print(show)
+    return jsonify(show)
 
-    return movies
+
+def fill_to_watch_list(is_movie):
+    discover = tmdb.Discover()
+    watch_list = []
+    page = random.randint(1, 500)
+    if(is_movie):
+        watch_list.append(discover.movie(page=page, language='EN')['results'])
+    else:
+        watch_list.append(discover.tv(page=page, language='EN')['results'])
+
+    watch_list = list(itertools.chain.from_iterable(watch_list))
+    return watch_list
 
 
 def get_random_movie(movieList):
--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,75 @@
 function main() {
     $('#getRandomMovie').click(getRandomMovie);
+    movieOrShow();
+    changeGenreList();
 }
 
+function changeGenreList() {
+    $('#show-cb').click(function () {
+        $('#movie-genres-select').css('display', 'none');
+        $('#shows-genres-select').css('display', 'block');
+    })
+    $('#movie-cb').click(function () {
+        $('#movie-genres-select').css('display', 'block');
+        $('#shows-genres-select').css('display', 'none');
+    })
+}
+
+
+function getSelectedGenre() {
+
+}
+
+//movie or tv show
+function getSelectedType() {
+    if ($("#movie-cb").prop("checked")) {
+        return "movie";
+    } else {
+        return "tv";
+    }
+}
+
+
 function getRandomMovie() {
+    let url = "";
+    if (getSelectedType() == "movie") {
+        url = '/get/movie';
+    } else {
+        url = 'get/tv';
+    }
+
     $('#movie').html("");
     $('#loader').css('display', 'block');
-    const url = '/get/movie';
-    $.getJSON(url, function (response) {
-        $('#loader').css('display', 'none');
-        console.log(response);
-        printMovieData(response);
+    $.ajax({
+        type: "POST",
+        url: url,
+        success: function (response) {
+            $('#loader').css('display', 'none');
+            console.log(response);
+            printMovieData(response);
+        }
     });
 }
 
-function printMovieData(movieData){
+function printMovieData(movieData) {
     $('#movie').html("");
     movieHtml =
-    `
+        `
         <img src="http://image.tmdb.org/t/p/w185/${movieData.poster_path}">
         <div id="movie-inner">
             <h2>${movieData.title}</h2>
-            <p>${movieData.release_date.substr(0,4)}<span>Score: ${movieData.vote_average}</span></p>
+            <p>${movieData.release_date.substr(0, 4)}<span>Score: ${movieData.vote_average}</span></p>
             <p>${movieData.overview}</p>
         </div>    
-    
     `;
 
     $('#movie').html(movieHtml);
+}
+
+function movieOrShow() {
+    $('input.type-checkbox').on('change', function () {
+        $('input.type-checkbox').not(this).prop('checked', false);
+    });
 }
 
 main();
